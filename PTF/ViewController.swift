d97b87f4781ea2025c2b5005567a0e024a154944
iRoma <roman.romanenko@progforce.com>
2016-06-07 21:03:38 +0300

    Initial Commit

---
+++ PTF/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PTF
+//
+//  Created by Roman Romanenko on 6/7/16.
+//  Copyright © 2016 Roman Romanenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
